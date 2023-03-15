2f8a26702bffb51cd73bf130ecc2ee22f69d1bfc
nthanhquyen02gmail.com <1050080114@sv.hcmunre.edu.vn>
2023-03-15 13:28:42 +0700

    baitapchuong4

---
+++ câu1/bt/tuần5/CamSanh.java
@@ -0,0 +1,12 @@
+
+package bt.tuần5;
+
+public class CamSanh extends QuaCam {
+    public String xuattencamsanh(){
+        return ten="Cam Sanh`";
+    }
+    public double giabancamsanh(){
+        super.setgiaban(45000);
+        return this.getgiaban()*this.getcannang();
+    }
+}
---
+++ câu2/bt tuần5/src/bt/tuần5/QuaCam.java
@@ -0,0 +1,13 @@
+package bt.tuần5;
+
+public class QuaCam extends HoaQua {
+    public String mau="Cam";
+    public String vi="chua";
+    public String xuattencam(){
+        return ten="Cam";
+    }
+    public double giabancam(){
+        super.setgiaban(18000);
+        return this.getcannang()*this.getgiaban();
+    }
+}
